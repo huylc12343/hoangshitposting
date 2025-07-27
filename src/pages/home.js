44786ac10e0a0a9548b7c75a5e97937ec1d749e8
huylc12343 <huylc12343@gmail.com>
2025-07-27 21:44:45 +0700

    chinh la nut di chuyen xuong

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,29 +4,32 @@ import HeroSection2 from '../components/HeroSection2';
 import HeroSection3 from '../components/HeroSection3';
 import AutoScrollBanner from '../components/AutoScroll';
 import Merch from '../components/MerchSection';
-import TimelineHeroSection from '../components/Timeline_Herosection'; // ✅ đã dùng đúng tên file
+import TimelineHeroSection from '../components/Timeline_Herosection';
 import DonViDongHanh from '../components/dv_DongHanh_HeroSection';
 import AutoScroll2 from '../components/AutoScroll2';
 import imgf from '../assets/join_us-min.jpg';
 import HoverImageWithButton from '../components/HoverImage';
-const Home = () => {
-  const timelineRef = useRef(null);
 
-  const scrollToTimeline = () => {
-    timelineRef.current?.scrollIntoView({ behavior: 'smooth' });
+const Home = () => {
+  const section2Ref = useRef(null);
+
+  const scrollToSection2 = () => {
+    section2Ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <div>
-      <HeroSection onScrollToTimeline={scrollToTimeline} />
-      <HeroSection2 />
+      <HeroSection onScrollToTimeline={scrollToSection2} />
+      <div ref={section2Ref}>
+        <HeroSection2 />
+      </div>
       <AutoScrollBanner />
       <HeroSection3 />
-      <AutoScroll2/>
-      <TimelineHeroSection ref={timelineRef} />
+      <AutoScroll2 />
+      <TimelineHeroSection />
       <Merch />
-      <DonViDongHanh/>
-      <HoverImageWithButton/>
+      <DonViDongHanh />
+      <HoverImageWithButton />
     </div>
   );
 };
