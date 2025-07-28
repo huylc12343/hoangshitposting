cda4a02ff8c077317074404425cc7a1cd0d8df9d
huylc12343 <huylc12343@gmail.com>
2025-07-28 14:53:16 +0700

    sua routing

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <ScrollToTop />
 
         <Routes>
-          <Route path="/hoangshitposting" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/merch" element={<Merch />} />
           <Route path="/upcoming" element={<Upcoming />} />
