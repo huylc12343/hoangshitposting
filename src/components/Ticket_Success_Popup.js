ac27042d95ee255b9bf7f3c26b8f9b482841fa66
huylc12343 <huylc12343@gmail.com>
2025-08-04 16:23:35 +0700

    update more popup

---
+++ src/components/Ticket_Success_Popup.js
@@ -0,0 +1,64 @@
+import React from "react";
+import warningIcon from "../assets/Success_Popup_img.png"; // thay bằng ảnh phù hợp
+import { useTheme } from "../contexts/Theme";
+import { useNavigate } from "react-router-dom";
+
+export default function Ticket_Success_Popup({
+  isOpen,
+  onClose,
+  onConfirm,
+  message = "Bạn có chắc chắn muốn tiếp tục?",
+}) {
+  const { theme } = useTheme();
+  const navigate = useNavigate();
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-[#323232] text-black  rounded-xl shadow-2xl p-6 w-[90%] max-w-md space-y-4 relative">
+        {/* Icon cảnh báo ở trên */}
+        <div className="w-full flex justify-center">
+          <img src={warningIcon} alt="Warning" className="w-[60%] h-[60%]" />
+        </div>
+
+        {/* Tiêu đề */}
+        <h2
+          className="text-3xl text-white font-bold text-center"
+          style={{
+            fontFamily: "Imbue, serif",
+          }}
+        >
+          Đặt vé thành công
+        </h2>
+
+        {/* Nội dung */}
+        <p className="text-center text-gray-400">
+          Cảm ơn bạn đã đồng hành cùng chúng mình. Email xác nhận sẽ được gửi
+          đến bạn trong vòng 48h kể từ khi hoàn thành đơn hàng ạ.{" "}
+        </p>
+        <p className="text-center text-gray-400">
+          Liên hệ fanpage: Hoangshitposting nếu có cần được hỗ trợ nha.{" "}
+        </p>
+
+        {/* 2 nút xếp thành 1 cột */}
+        <div className="flex flex-col gap-4 pt-4">
+          <button
+            style={{
+              backgroundColor: theme.color,
+            }}
+            onClick={() => {
+              onConfirm();
+              onClose();
+
+              navigate("/");
+            }}
+            className=" hover:opacity-80 w-full py-4 rounded text-white text-sm font-semibold"
+          >
+            Quay về trang chủ
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/timeOut_Popup.js
@@ -0,0 +1,50 @@
+import React from "react";
+import warningIcon from "../assets/timeout_popup.png";
+import { useTheme } from "../contexts/Theme";
+
+export default function TimeOut_Popup({ isOpen, onClose }) {
+  const { theme } = useTheme();
+  if (!isOpen) return null;
+
+  // Ngăn sự kiện lan ra ngoài popup
+  const handlePopupClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+      onClick={onClose} // Click ra ngoài để đóng
+    >
+<div
+  className="bg-[#323232] text-black rounded-xl shadow-2xl p-6 w-[90%] max-w-md space-y-4 relative"
+  onClick={handlePopupClick} // Ngăn click bên trong popup
+>
+  <div className="w-full flex justify-center ">
+    <img src={warningIcon} alt="Success" className="w-[60%] h-[60%]" />
+  </div>
+  <h2 className="text-3xl text-[#EEE1D8] font-bold text-center font-imbue">
+    Hết thời gian đặt hàng
+  </h2>
+  {/* Nội dung */}
+  <div className="text-center text-base text-gray-400 space-y-2 sm:p-8 sm:pb-4">
+    <p>"Rồi thời gian cũng hết!"</p>
+    <p>Nếu bạn đã chuyển khoản - Đừng lo!</p>
+    <p>Hãy giữ minh chứng và thử lại sau nhé.</p>
+    <p>Hoặc liên hệ fanpage Hoangshitposting để được hỗ trợ nha.</p>
+  </div>
+
+  <div className="flex justify-center pt-2">
+    <button
+      onClick={onClose}
+      className="text-[#EEE1D8] px-4 py-2 w-full h-auto rounded text-base font-Averta-CY font-semibold hover:bg-gray-200"
+      style={{ backgroundColor: theme.color }}
+    >
+      Đóng
+    </button>
+  </div>
+</div>
+
+    </div>
+  );
+}
